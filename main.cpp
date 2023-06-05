c4a7710d80fd10c3ee7c130748c034e824c1d582
易禮志 <127189877+seal11037@users.noreply.github.com>
2023-06-05 22:32:31 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include "Minesweeper.h"
 
 int main() {
-    cout << "Welcome to Minesweeper!" << endl;
+    int choice=1;
+    while(choice==1){
+        cout << "Welcome to Minesweeper!" << endl;
 
-    while (true) {
-        cout << endl;
-        cout << "1.新遊戲：" << endl;
-        cout << "2.退出：" << endl;
-        cout << "輸入您的選擇：";
+        while (true) {
+            cout << endl;
+            cout << "1.新遊戲：" << endl;
+            cout << "2.退出：" << endl;
+            cout << "輸入您的選擇：";
+        
 
-        int choice;
-        cin >> choice;
+            cin >> choice;
 
-        if (choice == 1) {
-            Minesweeper game;
-            game.playGame();
-        } else if (choice == 2) {
-            break;
-        } else {
-            cout << "無效的選擇，再試一次" << endl;
+            if (choice == 1) {
+                Minesweeper game;
+             game.playGame();
+             } 
+            else if (choice == 2) {
+                break;
+            } 
+            else {
+                cout << "無效的選擇，再試一次" << endl;
+            }
         }
     }
-
     return 0;
 }
